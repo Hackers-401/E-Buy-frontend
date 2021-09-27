bcae176f24442a47e0e2db9f46958cbd1a69d039
YousefAbu-Jalboush <yousef.y.jalboush@gmail.com>
2021-09-27 22:48:38 +0300

    feat: work in Service Page

--- src/components/ServicePage.js
+++ src/components/ServicePage.js
@@ -1,12 +1,65 @@
+import Product from "./Product"
 
 const ServicePage = () => {
 
 
 
     return (
-     <div>
-         
-     </div>
+        <div className="container servicespage">
+            <div className="row">
+                <div class="col-sm-3 ">
+                    <div class="left-sidebar">
+                        <h2>Category</h2>
+                        <div class="panel-group category-products " id="accordian">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="#">Kids</a></h4>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="#">Fashion</a></h4>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="#">Households</a></h4>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="#">Interiors</a></h4>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="#">Clothing</a></h4>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="#">Bags</a></h4>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title"><a href="#">Shoes</a></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-9">
+                    <div className="row features_items">
+						<h2 class="title text-center">Features Items</h2>
+                        {
+                            <Product/>
+                        }
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
